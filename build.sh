349428321c626d8f70060db7caed1cf02f91967b
Ken Herron <kenneth.herron@verizondigitalmedia.com>
2019-06-27 11:43:38 -0400

    Build using the command-line tools SDK instead of requiring XCode.

--- build.sh
+++ build.sh
@@ -10,7 +10,8 @@ INCLUDE="-I ${JAVA_HOME}/include -I ${JAVA_HOME}/include/darwin"
 FRAMEWORK="-framework Cocoa"
 SRC="native/main.m"
 DST="src/main/resources/sh/tak/appbundler/JavaAppLauncher"
-SDK="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk"
+# SDK="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk"
+SDK=/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk
 ARCH="x86_64"
 MIN_MAC_OSX_VERSION="10.7"
 
